df700d3d84cccd0aff5339771e1a5b5419b99f49
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-10-07 20:18:50 +0900

    [Chore] MamberMain 라우팅 주소 변경

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/member/login" element={<MemberLogin />}/>
         <Route path="/member/mypage" element={<MemberMypage />} />
-        <Route path="/member/main" element={<MemberMain />} />
+        <Route path="/member" element={<MemberMain />} />
       </Routes>
     </div>
     </BrowserRouter>

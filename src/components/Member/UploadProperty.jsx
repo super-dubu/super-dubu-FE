4c3ca7cbabb93095551006719955ef3dcf4dd877
Chaerrrish <dhcofls0899@naver.com>
2024-11-10 20:50:03 +0900

    [Chore] 주소 검색 Daum API 추가

--- src/components/Member/UploadProperty.jsx
+++ src/components/Member/UploadProperty.jsx
@@ -1,13 +1,52 @@
-import React, { useState } from 'react';
+import { debounce } from 'lodash';
+import React, { useEffect, useState } from 'react';
 import Header from '../Member/MemberHeader';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import DaumPostModal from '../api/DaumPost';
 
 function UploadProperty() {
     const [selectedCheckbox, setSelectedCheckbox] = useState('');
+    const [isPostModalOpen, setPostModalOpen] = useState(false);
+    const [address, setAddress] = useState('');
+    const [detailAddress, setDetailAddress] = useState('');
+    const [debouncedDetailAddress, setDebouncedDetailAddress] = useState('');
+
+    // Debounced change handler
+    const debouncedChangeHandler = debounce((value) => {
+        setDebouncedDetailAddress(value);
+    }, 3000); // 3초 지연
 
     const handleCheckboxChange = (value) => {
         setSelectedCheckbox(value);
     };
+
+    const handlePostModal = () => {
+        setPostModalOpen(!isPostModalOpen);
+    };
+
+    const handleAddressComplete = (data) => {
+        setAddress(data.address);
+        setPostModalOpen(false);
+    };
+
+    const handleAddressChange = (e) => {
+        const value = e.target.value;
+        setDetailAddress(value); // 실시간으로 업데이트
+        debouncedChangeHandler(value); // 3초 지연 후 마지막 값 저장
+    };
+
+    useEffect(() => {
+        // Debounced function 취소를 위해 cleanup 함수 반환
+        return () => {
+            debouncedChangeHandler.cancel();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (debouncedDetailAddress) {
+            console.log("Debounced Detail Address:", debouncedDetailAddress); // 마지막 데이터만 로그 출력
+        }
+    }, [debouncedDetailAddress]);
 
     return (
         <div>
@@ -17,54 +56,63 @@ function UploadProperty() {
                 <InputTitle>기본 정보</InputTitle>
                 <Basic>
                     <Set>
-                    <BoldText>매물 분류</BoldText>
-                    <DropDown>
-                        <DropDownOption>원/투룸</DropDownOption>
-                        <DropDownOption>오피스텔</DropDownOption>
-                        <DropDownOption>아파트</DropDownOption>
-                        <DropDownOption>주택/빌라</DropDownOption>
-                        <DropDownOption>상가/사무실</DropDownOption>
-                    </DropDown>
+                        <BoldText>매물 분류</BoldText>
+                        <DropDown>
+                            <DropDownOption>원/투룸</DropDownOption>
+                            <DropDownOption>오피스텔</DropDownOption>
+                            <DropDownOption>아파트</DropDownOption>
+                            <DropDownOption>주택/빌라</DropDownOption>
+                            <DropDownOption>상가/사무실</DropDownOption>
+                        </DropDown>
                     </Set>
                     <Set>
-                    <BoldText>거래 종류</BoldText>
-                    <Label>
-                        <CheckBox
-                            type="checkbox"
-                            checked={selectedCheckbox === '매매'}
-                            onChange={() => handleCheckboxChange('매매')}
-                        />
-                        매매
-                    </Label>
-                    <Label>
-                        <CheckBox
-                            type="checkbox"
-                            checked={selectedCheckbox === '전세'}
-                            onChange={() => handleCheckboxChange('전세')}
-                        />
-                        전세
-                    </Label>
-                    <Label>
-                        <CheckBox
-                            type="checkbox"
-                            checked={selectedCheckbox === '월세'}
-                            onChange={() => handleCheckboxChange('월세')}
-                        />
-                        월세
-                    </Label>
+                        <BoldText>거래 종류</BoldText>
+                        <Label>
+                            <CheckBox
+                                type="checkbox"
+                                checked={selectedCheckbox === '매매'}
+                                onChange={() => handleCheckboxChange('매매')}
+                            />
+                            매매
+                        </Label>
+                        <Label>
+                            <CheckBox
+                                type="checkbox"
+                                checked={selectedCheckbox === '전세'}
+                                onChange={() => handleCheckboxChange('전세')}
+                            />
+                            전세
+                        </Label>
+                        <Label>
+                            <CheckBox
+                                type="checkbox"
+                                checked={selectedCheckbox === '월세'}
+                                onChange={() => handleCheckboxChange('월세')}
+                            />
+                            월세
+                        </Label>
                     </Set>
                 </Basic>
                 <InputTitle>소재지</InputTitle>
                 <Basic>
                     <Set>
-                        <BoldText>소재지 주소(도로명)</BoldText>
-                        <StringInput />
+                        <BoldText>주소</BoldText>
+                        <StringInput variant="middle" value={address} placeholder={address || '기본 주소'} readOnly />
+                        <AddressButton onClick={handlePostModal}>주소 검색</AddressButton>
                     </Set>
                     <Set>
                         <BoldText>상세 주소</BoldText>
-                        <StringInput />
+                        <StringInput
+                            variant="long"
+                            value={detailAddress}
+                            onChange={handleAddressChange}
+                            placeholder={detailAddress || '상세 주소'} // 실시간으로 업데이트
+                        />
                     </Set>
                 </Basic>
+                {isPostModalOpen && (
+                    <DaumPostModal onComplete={handleAddressComplete} onClose={handlePostModal} />
+                )}
             </Container>
         </div>
     );
@@ -100,20 +148,16 @@ const Basic = styled.div`
     height: 5rem;
     flex-direction: column;
     justify-content: center;
-    /* align-items: center; */
-    /* gap: 1rem; */
     border-style: solid;
     border-width: 1px 1px 0 1px;
-
 `;
 
 const Set = styled.div`
     display: flex;
     flex-direction: row;
     height : 2.5rem;
-    border-bottom: 1px solid #000; /* 선 추가 */
-    align-items: center; /* 텍스트와 드롭다운, 체크박스를 세로 중앙에 정렬 */
-
+    border-bottom: 1px solid #000;
+    align-items: center;
 `;
 
 const BoldText = styled.div`
@@ -130,8 +174,7 @@ const DropDown = styled.select`
 
 const DropDownOption = styled.option``;
 
-const CheckBox = styled.input`
-`;
+const CheckBox = styled.input``;
 
 const Label = styled.label`
     margin-right: 2rem;
@@ -142,5 +185,20 @@ const StringInput = styled.input`
     border-color: #848484;
     border-width: 0.8px;
     height: 1.5rem;
-    width: 80%;
+    ${({ variant }) =>
+    variant === 'long'
+      ? css`
+          width: 300px;
+        `
+      : css`
+          width: 225px;
+        `}
+`;
+
+const AddressButton = styled.button`
+    background-color: white;
+    border-width: 1px;
+    margin-left: 10px;
+    height: 1.7rem;
+    border-radius: 5px;
 `;
